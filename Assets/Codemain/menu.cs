7c19a16d72b36f83eb30df4acf8cf7f58f891d40
khanhhuydoan <khanhhuy15022005@gmail.com>
2026-06-01 11:36:33 +0700

    Update menu.cs

--- Assets/Codemain/menu.cs
+++ Assets/Codemain/menu.cs
@@ -3,31 +3,34 @@ using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
+    // 
    public void Batdau ()
     {
         SceneManager.LoadScene(1);
     }
     public void ThoatGame()
     {
-        // In ra dòng ch? này ? c?a s? Console ?? b?n bi?t nút ?ã b?m thành công
+        // In ra ?? xác ??nh thoát thành công
         Debug.Log("?ã b?m nút Thoát Game!");
 
-        //thoát game (Ch? ho?t ??ng khi ?ã Build ra file game th?t)
+        //thoát game 
         Application.Quit();
-
-        //  t?t ch? ?? Play ngay bên trong ph?n m?m Unity Editor
+        //thoát ch? ?? ch?y th?
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
     }
+    // tr? v?
     public void trove ()
     {
         SceneManager.LoadScene(0);
     }
+    //vào quiz
     public void QUIZ()
     {
         SceneManager.LoadScene(4);
     }
+    //vào ch? ?? alphabet
     public void ALPHABET()
     {
         SceneManager.LoadScene(2);
